e50080ed861158d17ff8f2a20eb59709a920b1d3
Sean Daly <daly.se91@gmail.com>
2021-02-02 22:01:04 -0600

    Troubleshooting heroku error, unhandledpromise

--- controllers/Item.js
+++ controllers/Item.js
@@ -3,31 +3,35 @@ const router = express.Router();
 const Item = require("../models/Item");
 const List = require("../models/List");
 // Index Route ------------------------------------------
-router.get("/", async (req, res) => {
-  res.json(await Item.find({}));
-  await returnsPromise().catch(e => { console.log(e) })
-});
+router.get('/',   (req, res) => {
+  Item.find({}).then(allItems =>{
+      res.json({
+          items: allItems
+      })
+  }).catch( err => {res.json({status: 400})
+})
+})
 // Create Routes ----------------------------------------
-router.post("/:listId", async (req, res) => {
-    const list = await List.findById(req.params.listId)
+// router.post("/:listId", async (req, res) => {
+//     const list = await List.findById(req.params.listId)
     
-    const item = await Item.create(req.body)
-    list.items.push(item._id)
-    list.save()
-    res.json(list.populate("items"))
-    await returnsPromise().catch(e => { console.log(e) })
-});
-// Update Routes ----------------------------------------
-router.put("/:id", async (req, res) => {
-  res.json(
-    await Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    )
-    await returnsPromise().catch(e => { console.log(e) })
-});
-// Destroy Routes ----------------------------------------
-router.delete("/:id", async (req, res) => {
-  res.json(await Item.findByIdAndRemove(req.params.id));
-  await returnsPromise().catch(e => { console.log(e) })
-});
+//     const item = await Item.create(req.body)
+//     list.items.push(item._id)
+//     list.save()
+//     res.json(list.populate("items"))
+    
+// });
+// // Update Routes ----------------------------------------
+// router.put("/:id", async (req, res) => {
+//   res.json(
+//     await Item.findByIdAndUpdate(req.params.id, req.body, { new: true })
+//     )
+    
+// });
+// // Destroy Routes ----------------------------------------
+// router.delete("/:id", async (req, res) => {
+//   res.json(await Item.findByIdAndRemove(req.params.id));
+  
+// });
 
-module.exports = router;
+module.exports = router
