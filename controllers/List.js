7e55573ff634225d4994b54aeaa19abb12509a78
Sean Daly <daly.se91@gmail.com>
2021-02-02 22:05:53 -0600

    updated list controller to troubleshoot heroku error

--- controllers/List.js
+++ controllers/List.js
@@ -4,25 +4,30 @@ const List = require("../models/List");
 const Item = require("../models/Item")
 
 // Index Route ------------------------------------------
-router.get("/", async (req, res) => {
-  res.json(await List.find({}).populate('items'));
-});
+router.get('/',   (req, res) => {
+  List.find({}).then(allLists =>{
+      res.json({
+          lists: allLists
+      })
+  }).catch( err => {res.json({status: 400})
+})
+})
 // Create Routes ----------------------------------------
-router.post("/", async (req, res) => {
-  res.json(await List.create(req.body));    
-});
+// router.post("/", async (req, res) => {
+//   res.json(await List.create(req.body));    
+// });
 
 
-// Update Routes ----------------------------------------
-router.put("/:id", async (req, res) => {
-  res.json(
-    await List.findByIdAndUpdate(req.params.id, req.body, { new: true })
-  );
-});
+// // Update Routes ----------------------------------------
+// router.put("/:id", async (req, res) => {
+//   res.json(
+//     await List.findByIdAndUpdate(req.params.id, req.body, { new: true })
+//   );
+// });
 
-// Destroy Routes ----------------------------------------
-router.delete("/:id", async (req, res) => {
-  res.json(await List.findByIdAndRemove(req.params.id));
-});
+// // Destroy Routes ----------------------------------------
+// router.delete("/:id", async (req, res) => {
+//   res.json(await List.findByIdAndRemove(req.params.id));
+// });
 
 module.exports = router;
